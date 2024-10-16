3714cfc385718939cae85e803b95dcea86c5690e
Renan Alberto de Souza <20868822+renansouza-dev@users.noreply.github.com>
2024-10-17 00:11:24 +0100

    fix: remove unused import.

--- folio-app-accounts/src/test/java/com/renansouza/folio/accounts/AccountsServiceTest.java
+++ folio-app-accounts/src/test/java/com/renansouza/folio/accounts/AccountsServiceTest.java
@@ -12,7 +12,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.verification.VerificationMode;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
