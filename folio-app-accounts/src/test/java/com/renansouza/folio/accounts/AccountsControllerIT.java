0951e282c050c544475f7b7f82d4c896a8ab59a2
Renan Alberto de Souza <20868822+renansouza-dev@users.noreply.github.com>
2024-12-26 16:08:13 +0000

    update dependecies and fix IT testing.

--- folio-app-accounts/src/test/java/com/renansouza/folio/accounts/AccountsControllerIT.java
+++ folio-app-accounts/src/test/java/com/renansouza/folio/accounts/AccountsControllerIT.java
@@ -28,6 +28,8 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static com.renansouza.folio.accounts.AccountUtils.getEntities;
 import static com.renansouza.folio.accounts.AccountUtils.getFailureRequest;
@@ -45,6 +47,7 @@ class AccountsControllerIT {
     @LocalServerPort
     private Integer port;
 
+    @Container
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.3-alpine");
 
     @BeforeAll
