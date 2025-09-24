98007773270c28e28020aa74197215ef5c108e62
Renan Souza <20868822+renansouza-dev@users.noreply.github.com>
2025-09-24 23:36:00 +0100

    chore: revert java 25 changes.

--- folio-app-transactions/src/main/java/com/renansouza/folio/transactions/TransactionsApplication.java
+++ folio-app-transactions/src/main/java/com/renansouza/folio/transactions/TransactionsApplication.java
@@ -14,7 +14,7 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 public class TransactionsApplication {
 
   @Generated
-  static void main(String[] args) {
+  public static void main(String[] args) {
     SpringApplication.run(TransactionsApplication.class, args);
   }
 
